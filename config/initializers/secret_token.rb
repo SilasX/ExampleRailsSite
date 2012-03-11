cc864e08cf9fc78c13d5a8a8b7bb13b1d0288c4e
Silas Barta <sbarta@gmail.com>
2012-03-10 23:20:04 -0800

    initialize with Devise, root not set

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleRailsSite::Application.config.secret_token = '1522e13d6fc421eea23a6fcfa61315bed2ab60f15bf87cd7361ac807f719b66acd34d72703cad0ce0babc99992bbc1ddf9fdb9b5321c2d82a128c32e81775a6f'
